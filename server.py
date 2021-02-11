aaa5b9b3f40bbe18ec4fca048d2b1002b6b8a64f
unknown <bridibusher@gmail.com>
2021-02-10 21:00:37 -0800

    Added addTeam endpoint

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import scoped_session, sessionmaker
 import traceback
@@ -10,3 +10,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "test"
+
+@app.route("/addTeam", methods = ["POST"])
+def addTeam():
+    addTeam_params = request.get_json()
+    team_name = str(addTeam_params["team_name"])
+    if(db.execute("SELECT COUNT(team_name) FROM teams WHERE upper(team_name) =:team_name", {"team_name":team_name.upper()}) != 0):
+        db.execute("INSERT INTO teams (team_name) VALUES(:team_name",{"team_name":team_name})
+        return jsonify({"error":"success"}),200
+
+
